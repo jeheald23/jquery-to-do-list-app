2ad338a0c83f1c2d6a2d00850d4ea69ba2ff0b0a
jeheald23 <jeheald23@gmail.com>
2023-10-19 11:30:11 +0200

    fixed errors

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ function newItem(){
 
     //javascript
     //1. Adding a new item to the list of items: 
-       let li = $("<li></li>");
-       let inputValue = $("input").val();
-       li.appendChild(inputValue);
-    
-       if (inputValue === '') {
-         alert("You must write something!");
-       } else {
-        $('#list').append(li);
-       }
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+  
+    if (inputValue === '') {
+      alert("You must write something!");
+    } else {
+      $('#list').append(li);
+    }
     
      //2. Crossing out an item from the list of items:
      function crossOut() {
